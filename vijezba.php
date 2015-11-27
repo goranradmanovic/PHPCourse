858e58d642920fcb4ce1071c7249f46a51b5dc5f
Goran R <goranradmanovic@gmail.com>
2015-11-27 21:53:40 +0100

    Adding HTML <pre> tags and more coments and echo outputs

--- vijezba.php
+++ vijezba.php
@@ -1,41 +1,51 @@
 <?php
 
-//Numericki niz
+echo '<pre>'; //Ispisujemo HTML <pre> tag za formatiranje coda i nizova
 
+//Stvaramo Numericki niz
 $fruit = array('Apple', 'Pears', 'Bananas', 'Lemon', 'Orange');
 
-//Asocijativni niz
-
+//Stvaramo Asocijativni niz
 $cars = array('Ferrari' => 'Modena', 'AstonMartin' => 'Vanquis', 'Bently' => 'Continental GT', 'Audi' => 'R8');
 
 //Ispis sadrzaja nizova uz pomoci var_dump() funkc.
 
-var_dump($fruit); #Ispis numerickog niza
+echo "Ispis numerickog niza sa var_dump() funk.\n";
+echo var_dump($fruit) , "\n"; #Ispis numerickog niza
 
-var_dump($cars); #Ispis asocijativnog niza
+echo "Ispis asocijativnog niza sa var_dump() funk.\n";
+echo var_dump($cars) , "\n"; #Ispis asocijativnog niza
 
 //Ispis pojednog el. niza
 
-var_dump($fruit[1]); #Ispis numerickog niza
+echo "Ispis jednog el. numerickog niza sa var_dump() funk.\n";
+echo var_dump($fruit[1]) , "\n"; #Ispis jednog el. numerickog niza
 
-var_dump($cars['AstonMartin']); #Ispis asocijativnog niza
+echo "Ispis jednog el. asocijativnog niza sa var_dump() funk.\n";
+echo var_dump($cars['AstonMartin']) , "\n"; #Ispis jednog el. asocijativnog niza
 
 //Ispis sa echo u novoj liniji
 
-echo "$fruit[1]\n"; #Ispis numerickog niza
+echo "Ispis jednog el. numerickog niza\n";
+echo $fruit[1] , "\n\n"; #Ispis jednog el. numerickog niza
 
-echo "$cars[AstonMartin]\n"; #Ispis asocijativnog niza
+echo "Ispis jednog el. asocijativnog niza\n";
+echo $cars['AstonMartin'] , "\n\n"; #Ispis jednog el. asocijativnog niza
 
-//Multidimenzionalni array tj. niz
-
+//Stvaramo Multidimenzionalni array tj. niz
 $food = array('Healty' => array('Salat', 'Vegetables', 'Fruit'), 'Unhealty' => array('Pizza', 'Burger', 'HotDog'));
 
-var_dump($food); #Ispis multidimenzonalnog niza sa var_dump
+echo "Ispis multidimenzonalnog niza sa var_dump() funk.";
+echo var_dump($food) , "\n"; #Ispis multidimenzonalnog niza sa var_dump
 
 //Ispis pojednog el. multidimenzonalnog niza sa echo
+
+echo "Ispis pojedinacnih el. multidimenzonalnog niza.\n\n";
 
 echo 'First el. of Healty array is ' , $food['Healty'][0] , "\n"; #Zdrava hrana
 
 echo 'Second el. of Unhealty array is ' , $food['Unhealty'][2] , "\n"; #Nezdrava hrana
+
+echo '</pre>'; #Zatvaramo HTML <pre> tag za formatiranje coda i nizova
 
 ?>
